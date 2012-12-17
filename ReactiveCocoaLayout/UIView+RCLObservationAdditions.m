07fb5129f9c20a38e5a8b7e8ccf35a92aa1e688a
Justin Spahr-Summers <Justin.SpahrSummers@gmail.com>
2012-12-17 00:25:10 -0800

    Add UIView.rcl_intrinsicContentSizeSignal

--- ReactiveCocoaLayout/UIView+RCLObservationAdditions.m
+++ ReactiveCocoaLayout/UIView+RCLObservationAdditions.m
@@ -7,8 +7,34 @@
 //
 
 #import "UIView+RCLObservationAdditions.h"
+#import "RACSignal+RCLSignalAdditions.h"
+#import <objc/runtime.h>
+
+// Associated with a RACSubject which sends -intrinsicContentSize whenever
+// -invalidateIntrinsicContentSize is invoked.
+static void *UIViewIntrinsicContentSizeSubjectKey = &UIViewIntrinsicContentSizeSubjectKey;
+
+static void (*oldInvalidateIntrinsicContentSize)(UIView *, SEL);
+static void newInvalidateIntrinsicContentSize(UIView *self, SEL _cmd) {
+	oldInvalidateIntrinsicContentSize(self, _cmd);
+
+	RACSubject *subject = objc_getAssociatedObject(self, UIViewIntrinsicContentSizeSubjectKey);
+	if (subject == nil) return;
+
+	[subject sendNext:MEDBox(self.intrinsicContentSize)];
+}
 
 @implementation UIView (RCLObservationAdditions)
+
++ (void)load {
+	SEL selector = @selector(invalidateIntrinsicContentSize);
+
+	Method method = class_getInstanceMethod(self, selector);
+	NSAssert(method != NULL, @"Could not find %@ on %@", NSStringFromSelector(selector), self);
+
+	oldInvalidateIntrinsicContentSize = (__typeof__(oldInvalidateIntrinsicContentSize))method_getImplementation(method);
+	class_replaceMethod(self, selector, (IMP)&newInvalidateIntrinsicContentSize, method_getTypeEncoding(method));
+}
 
 // FIXME: These properties aren't actually declared as KVO-compliant by UIKit.
 // Here be dragons?
@@ -18,6 +44,16 @@
 
 - (id)rcl_frameSignal {
 	return RACAbleWithStart(self.frame);
+}
+
+- (id<RCLSignal>)rcl_intrinsicContentSizeSignal {
+	RACSubject *subject = objc_getAssociatedObject(self, UIViewIntrinsicContentSizeSubjectKey);
+	if (subject == nil) {
+		subject = [RACSubject subject];
+		objc_setAssociatedObject(self, UIViewIntrinsicContentSizeSubjectKey, subject, OBJC_ASSOCIATION_RETAIN_NONATOMIC);
+	}
+
+	return [subject startWith:MEDBox(self.intrinsicContentSize)];
 }
 
 @end
