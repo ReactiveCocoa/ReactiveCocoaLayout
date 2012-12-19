9da9fbe831e8dd80afc407a62c05aa83c4ca14a1
Justin Spahr-Summers <Justin.SpahrSummers@gmail.com>
2012-12-18 22:27:15 -0800

    Rename xs -> minXs, ys -> minYs in unit tests

--- ReactiveCocoaLayoutTests/RACSignalRCLGeometryAdditionsSpec.m
+++ ReactiveCocoaLayoutTests/RACSignalRCLGeometryAdditionsSpec.m
@@ -15,8 +15,8 @@ __block RACSequence *points;
 __block RACSequence *widths;
 __block RACSequence *heights;
 
-__block RACSequence *xs;
-__block RACSequence *ys;
+__block RACSequence *minXs;
+__block RACSequence *minYs;
 
 __block RACSequence *maxXs;
 __block RACSequence *maxYs;
@@ -44,19 +44,19 @@ beforeEach(^{
 		return MEDBox(value.med_rectValue.origin);
 	}];
 
-	xs = [points map:^(NSValue *value) {
+	minXs = [points map:^(NSValue *value) {
 		return @(value.med_pointValue.x);
 	}];
 
-	ys = [points map:^(NSValue *value) {
+	minYs = [points map:^(NSValue *value) {
 		return @(value.med_pointValue.y);
 	}];
 
-	maxXs = [RACSequence zip:@[ xs, widths ] reduce:^(NSNumber *x, NSNumber *width) {
+	maxXs = [RACSequence zip:@[ minXs, widths ] reduce:^(NSNumber *x, NSNumber *width) {
 		return @(x.doubleValue + width.doubleValue);
 	}];
 
-	maxYs = [RACSequence zip:@[ ys, heights ] reduce:^(NSNumber *y, NSNumber *height) {
+	maxYs = [RACSequence zip:@[ minYs, heights ] reduce:^(NSNumber *y, NSNumber *height) {
 		return @(y.doubleValue + height.doubleValue);
 	}];
 });
@@ -86,18 +86,18 @@ describe(@"signal of CGRects", ^{
 
 	it(@"should map to positions of a specific edge", ^{
 		RACSignal *minX = [signal positionOfEdge:[RACSignal return:@(CGRectMinXEdge)]];
-		expect(minX.sequence).to.equal(xs);
+		expect(minX.sequence).to.equal(minXs);
 
 		RACSignal *minY = [signal positionOfEdge:[RACSignal return:@(CGRectMinYEdge)]];
-		expect(minY.sequence).to.equal(ys);
+		expect(minY.sequence).to.equal(minYs);
 	});
 
 	it(@"should map to minX values", ^{
-		expect(signal.minX.sequence).to.equal(xs);
+		expect(signal.minX.sequence).to.equal(minXs);
 	});
 
 	it(@"should map to minY values", ^{
-		expect(signal.minY.sequence).to.equal(ys);
+		expect(signal.minY.sequence).to.equal(minYs);
 	});
 
 	it(@"should map to maxX values", ^{
@@ -112,11 +112,11 @@ describe(@"signal of CGRects", ^{
 		RACSequence *leading = signal.leading.sequence;
 		RACSequence *trailing = signal.trailing.sequence;
 
-		if ([leading isEqual:xs]) {
+		if ([leading isEqual:minXs]) {
 			expect(trailing).to.equal(maxXs);
 		} else {
 			expect(leading).to.equal(maxXs);
-			expect(trailing).to.equal(xs);
+			expect(trailing).to.equal(minXs);
 		}
 	});
 
@@ -284,12 +284,12 @@ describe(@"signal of CGPoints", ^{
 		signal = [points signalWithScheduler:RACScheduler.immediateScheduler];
 	});
 
-	it(@"should map to Xs", ^{
-		expect(signal.x.sequence).to.equal(xs);
+	it(@"should map to minXs", ^{
+		expect(signal.x.sequence).to.equal(minXs);
 	});
 
-	it(@"should map to Ys", ^{
-		expect(signal.y.sequence).to.equal(ys);
+	it(@"should map to minYs", ^{
+		expect(signal.y.sequence).to.equal(minYs);
 	});
 
 	it(@"should be returned from +pointsWithX:Y:", ^{
@@ -321,7 +321,7 @@ describe(@"+min: and +max:", ^{
 	beforeEach(^{
 		signals = @[
 			[widths signalWithScheduler:RACScheduler.immediateScheduler],
-			[ys signalWithScheduler:RACScheduler.immediateScheduler],
+			[minYs signalWithScheduler:RACScheduler.immediateScheduler],
 		];
 	});
 
