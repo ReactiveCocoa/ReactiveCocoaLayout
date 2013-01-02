72107e96bd587dbc22c5c2fbe574ac6cc472f89b
Justin Spahr-Summers <Justin.SpahrSummers@gmail.com>
2013-01-01 22:20:17 -0800

    Add names to UIView signals

--- ReactiveCocoaLayout/UIView+RCLGeometryAdditions.m
+++ ReactiveCocoaLayout/UIView+RCLGeometryAdditions.m
@@ -69,42 +69,53 @@ static CGRect backingAlignedRect(UIView *view, CGRect rect) {
 - (RACSignal *)rcl_boundsSignal {
 	@weakify(self);
 
-	return [[RACAbleWithStart(self.layer.bounds)
+	RACSignal *signal = [[RACAbleWithStart(self.layer.bounds)
 		map:^(id _) {
 			@strongify(self);
 			return MEDBox(self.bounds);
 		}]
 		distinctUntilChanged];
+	
+	signal.name = [NSString stringWithFormat:@"%@ -rcl_boundsSignal", self];
+	return signal;
 }
 
 - (RACSignal *)rcl_frameSignal {
 	@weakify(self);
 
-	return [[[RACSignal merge:@[ self.rcl_boundsSignal, RACAbleWithStart(self.layer.position) ]]
+	RACSignal *signal = [[[RACSignal merge:@[ self.rcl_boundsSignal, RACAbleWithStart(self.layer.position) ]]
 		map:^(id _) {
 			@strongify(self);
 			return MEDBox(self.frame);
 		}]
 		distinctUntilChanged];
+	
+	signal.name = [NSString stringWithFormat:@"%@ -rcl_frameSignal", self];
+	return signal;
 }
 
 - (RACSignal *)rcl_baselineSignal {
+	RACSignal *signal;
+
 	if (self.viewForBaselineLayout == self) {
 		// The baseline will always be the bottom of our bounds.
-		return [RACSignal return:@0];
+		signal = [RACSignal return:@0];
+	} else {
+		@weakify(self);
+		signal = [[RACSignal
+			merge:@[ self.rcl_boundsSignal, self.rcl_frameSignal, self.viewForBaselineLayout.rcl_frameSignal ]]
+			map:^(id _) {
+				@strongify(self);
+
+				UIView *baselineView = self.viewForBaselineLayout;
+				NSAssert([baselineView.superview isEqual:self], @"%@ must be a subview of %@ to be its viewForBaselineLayout", baselineView, self);
+
+				return @(CGRectGetHeight(self.bounds) - CGRectGetMaxY(baselineView.frame));
+			}].distinctUntilChanged;
 	}
 
-	@weakify(self);
-	return [[RACSignal
-		merge:@[ self.rcl_boundsSignal, self.rcl_frameSignal, self.viewForBaselineLayout.rcl_frameSignal ]]
-		map:^(id _) {
-			@strongify(self);
-
-			UIView *baselineView = self.viewForBaselineLayout;
-			NSAssert([baselineView.superview isEqual:self], @"%@ must be a subview of %@ to be its viewForBaselineLayout", baselineView, self);
-
-			return @(CGRectGetHeight(self.bounds) - CGRectGetMaxY(baselineView.frame));
-		}].distinctUntilChanged;
+	signal.name = [NSString stringWithFormat:@"%@ -rcl_baselineSignal", self];
+	return signal;
 }
 
 @end
