07dca19d6de392a049397175463f29a4cd673897
Justin Spahr-Summers <Justin.SpahrSummers@gmail.com>
2013-01-04 03:28:11 -0800

    Some tests for the animated signal additions

---
+++ ReactiveCocoaLayoutTests/RACSignalRCLAnimationAdditionsSpec.m
@@ -0,0 +1,99 @@
+//
+//  RACSignalRCLAnimationAdditionsSpec.m
+//  ReactiveCocoaLayout
+//
+//  Created by Justin Spahr-Summers on 2013-01-04.
+//  Copyright (c) 2013 GitHub. All rights reserved.
+//
+
+SpecBegin(RACSignalRCLAnimationAdditions)
+
+__block RACSignal *baseSignal;
+
+beforeEach(^{
+	baseSignal = [RACSignal return:@0];
+});
+
+describe(@"RCLIsInAnimatedSignal()", ^{
+	it(@"should be false outside of an animated signal", ^{
+		expect(RCLIsInAnimatedSignal()).to.beFalsy();
+	});
+
+	it(@"should be true from nexts of -animate", ^{
+		[[[[baseSignal animate]
+			doNext:^(id x) {
+				expect(x).to.equal(@0);
+				expect(RCLIsInAnimatedSignal()).to.beTruthy();
+			}]
+			doCompleted:^{
+				expect(RCLIsInAnimatedSignal()).to.beFalsy();
+			}]
+			toArray];
+	});
+
+	it(@"should be true from nexts of -animateWithDuration:", ^{
+		[[[[baseSignal animateWithDuration:0.01]
+			doNext:^(id x) {
+				expect(x).to.equal(@0);
+				expect(RCLIsInAnimatedSignal()).to.beTruthy();
+			}]
+			doCompleted:^{
+				expect(RCLIsInAnimatedSignal()).to.beFalsy();
+			}]
+			toArray];
+	});
+
+	it(@"should be true from nexts of -animateWithDuration:curve:", ^{
+		[[[[baseSignal animateWithDuration:0.01 curve:RCLAnimationCurveEaseOut]
+			doNext:^(id x) {
+				expect(x).to.equal(@0);
+				expect(RCLIsInAnimatedSignal()).to.beTruthy();
+			}]
+			doCompleted:^{
+				expect(RCLIsInAnimatedSignal()).to.beFalsy();
+			}]
+			toArray];
+	});
+});
+
+describe(@"-doAnimationCompleted:", ^{
+	it(@"should trigger when the animation completes", ^{
+		__block BOOL animationCompleted = NO;
+		__block BOOL signalCompleted = NO;
+
+		[[[baseSignal
+			animate]
+			doAnimationCompleted:^(id x) {
+				animationCompleted = YES;
+			}]
+			subscribeCompleted:^{
+				signalCompleted = YES;
+			}];
+
+		expect(signalCompleted).to.beTruthy();
+		expect(animationCompleted).to.beFalsy();
+		expect(animationCompleted).will.beTruthy();
+	});
+
+	it(@"should behave like -doNext: outside of an animated signal", ^{
+		__block BOOL animationCompleted = NO;
+		__block BOOL signalCompleted = NO;
+
+		[[baseSignal
+			doAnimationCompleted:^(id x) {
+				expect(x).to.equal(@0);
+				animationCompleted = YES;
+			}]
+			subscribeNext:^(id x) {
+				expect(x).to.equal(@0);
+				expect(animationCompleted).to.beTruthy();
+			} completed:^{
+				signalCompleted = YES;
+			}];
+
+		expect(animationCompleted).to.beTruthy();
+		expect(signalCompleted).to.beTruthy();
+	});
+});
+
+SpecEnd
