02eff63130aac238c0d3a56e6a28cf19038f0de6
Justin Spahr-Summers <Justin.SpahrSummers@gmail.com>
2013-05-11 18:34:27 -0700

    Document RCLFrame and RCLAlignment
    Also, remove RCLBounds, because a lot of the layout attributes don't
    really make sense for it.

--- ReactiveCocoaLayout/RCLMacros.h
+++ ReactiveCocoaLayout/RCLMacros.h
@@ -8,12 +8,55 @@
 
 #import <ReactiveCocoa/metamacros.h>
 
+// Binds a view's frame to a set of attributes which describe different parts of
+// the frame rectangle.
+//
+// This macro should be used on the left side of an equal sign, and the right
+// side should be an NSDictionary containing RCL layout attributes, mapped to
+// the signals or constant values to bind them to.
+//
+// The order that attributes are specified is irrelevant because dictionaries
+// are always unordered. However, because there can be implicit dependencies in
+// layout calculations (e.g., aligning the right side of a rectangle requires
+// knowing its final width), attributes are applied in the following order:
+//
+//  1. `rcl_rect`
+//  2. `rcl_size`, `rcl_origin`
+//  3. `rcl_width`, `rcl_height`
+//  4. `rcl_center`
+//  5. `rcl_centerX`, `rcl_centerY`
+//  6. `rcl_left`, `rcl_top`, `rcl_right`, `rcl_bottom`, `rcl_leading`, `rcl_trailing`
+//
+// The relative order of attributes that have the same priority is undefined.
+//
+// Examples:
+//
+//  /*
+//   * Sets the top of the view's frame to a constant 8 points, and puts the
+//   * leading side of the rect after the trailing side of another view, plus
+//   * 6 points of padding.
+//   *
+//   * The view's frame will always match its intrinsic size.
+//   */
+//  RCLFrame(view) = @{
+//      rcl_top: @8,
+//      rcl_leading: [otherView.rcl_trailingSignal plus:[RACSignal return:@6]]
+//  };
+//
+//  /*
+//   * Keeps the view at a constant 64 points wide, and centered at (100, 100).
+//   *
+//   * The view's height will always match its intrinsic size.
+//   */
+//  RCLFrame(view) = @{
+//      rcl_width: @64,
+//      rcl_center: MEDBox(CGPointMake(100, 100))
+//  };
 #define RCLFrame(VIEW) \
 	[RCLRectAssignmentTrampoline trampolineWithView:(VIEW)][@"rcl_frame"]
 
-#define RCLBounds(VIEW) \
-	[RCLRectAssignmentTrampoline trampolineWithView:(VIEW)][@"rcl_bounds"]
-
+// Like `RCLFrame`, but binds to the view's `rcl_alignmentRect` instead of its
+// `rcl_frame`.
 #define RCLAlignment(VIEW) \
 	[RCLRectAssignmentTrampoline trampolineWithView:(VIEW)][@"rcl_alignmentRect"]
 
@@ -50,9 +93,9 @@ typedef enum : NSInteger {
 	// applied (and overwritten) in a binding.
 	RCLAttributeRect,
 	RCLAttributeSize,
+	RCLAttributeOrigin,
 	RCLAttributeHeight,
 	RCLAttributeWidth,
-	RCLAttributeOrigin,
 	RCLAttributeCenter,
 	RCLAttributeCenterX,
 	RCLAttributeCenterY,
