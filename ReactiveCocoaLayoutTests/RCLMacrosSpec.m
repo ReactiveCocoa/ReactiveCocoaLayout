0d06ba7db6dee2d932618bc6bcb124569ae2cca2
Justin Spahr-Summers <justin.spahrsummers@gmail.com>
2013-05-14 22:38:12 -0700

    Refactor RCLMacros test into a shared example group
    This way, we can test RCLFrame() and RCLAlignment() identically.

--- ReactiveCocoaLayoutTests/RCLMacrosSpec.m
+++ ReactiveCocoaLayoutTests/RCLMacrosSpec.m
@@ -8,22 +8,66 @@
 
 #import "TestView.h"
 
-SpecBegin(RCLMacros)
+static NSString * const MacroExamples = @"MacroExamples";
 
-__block TestView *view;
+// Associated with a block that binds a dictionary of attributes to the desired
+// view property. This block should be of type:
+//
+// void (^bind)(TestView *view, NSDictionary *attributes)
+static NSString * const MacroBindingBlock = @"MacroBindingBlock";
 
-beforeEach(^{
-	view = [[TestView alloc] initWithFrame:CGRectZero];
+// Associated with the name of the view property that is being bound.
+static NSString * const MacroPropertyName = @"MacroPropertyName";
+
+SharedExampleGroupsBegin(MacroExamples)
+
+sharedExamplesFor(MacroExamples, ^(NSDictionary *bindingInfo) {
+	__block TestView *view;
+
+	__block void (^bind)(NSDictionary *);
+	__block CGRect (^getProperty)(void);
+
+	beforeEach(^{
+		view = [[TestView alloc] initWithFrame:CGRectZero];
+
+		void (^innerBindingBlock)(TestView *, NSDictionary *) = bindingInfo[MacroBindingBlock];
+		bind = [^(NSDictionary *bindings) {
+			return innerBindingBlock(view, bindings);
+		} copy];
+
+		getProperty = [^{
+			NSValue *boxedRect = [view valueForKey:bindingInfo[MacroPropertyName]];
+			return boxedRect.med_rectValue;
+		} copy];
+	});
+
+	it(@"should bind constant values", ^{
+		bind(@{
+			rcl_rect: MEDBox(CGRectMake(0, 0, 10, 20)),
+			rcl_right: @15,
+			rcl_height: @30,
+		});
+
+		expect(getProperty()).to.equal(CGRectMake(5, 0, 10, 30));
+	});
 });
 
-it(@"should bind constant values", ^{
-	RCLFrame(view) = @{
-		rcl_rect: MEDBox(CGRectMake(0, 0, 10, 20)),
-		rcl_right: @15,
-		rcl_height: @30,
-	};
+SharedExampleGroupsEnd
 
-	expect(view.frame).to.equal(CGRectMake(5, 0, 10, 30));
+SpecBegin(RCLMacros)
+
+itShouldBehaveLike(MacroExamples, @{
+	MacroPropertyName: @"rcl_frame",
+	MacroBindingBlock: ^(TestView *view, NSDictionary *bindings) {
+		RCLFrame(view) = bindings;
+	}
+});
+
+itShouldBehaveLike(MacroExamples, @{
+	MacroPropertyName: @"rcl_alignmentRect",
+	MacroBindingBlock: ^(TestView *view, NSDictionary *bindings) {
+		RCLAlignment(view) = bindings;
+	}
 });
 
 SpecEnd
